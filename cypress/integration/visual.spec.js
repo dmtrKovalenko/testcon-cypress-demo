47cbcb6a655405c4ecdbc86bd67fc4a78c03f30e
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-10-11 15:30:10 +0300

    Snapshot also speakers page

--- cypress/integration/visual.spec.js
+++ cypress/integration/visual.spec.js
@@ -1,4 +1,9 @@
 describe("Visual regression", () => {
+  it("Shows speakers page", () => {
+    cy.visit("https://www.testcon.lt/speakers/")
+    cy.percySnapshot('Speakers')
+  })
+
   it("Shows main page", () => {
     cy.visit("https://www.testcon.lt/")
     cy.percySnapshot('Main')
